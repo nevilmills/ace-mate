383ba3a9cc653ce314d13542c4f926ce57ad16ec
Nevil Mills <nevilmills@gmail.com>
2024-09-20 19:02:46 -0700

    Finish basic login UI

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -9,6 +9,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import React from "react";
+import { Label } from "@/components/ui/label";
 
 interface pageProps {}
 
@@ -22,14 +23,47 @@ export const page: React.FC<pageProps> = ({}) => {
             Your latest golf statistics await...
           </CardDescription>
         </CardHeader>
-        <CardContent>
-          <div className="space-y-4">
-            <Input type="text" placeholder="Username" className="w-full" />
-            <Input type="password" placeholder="Password" className="w-full" />
+        <CardContent className="">
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name" className="font-semibold">
+                Username
+              </Label>
+              <Input
+                id="name"
+                type="text"
+                placeholder="Tiger Woods"
+                className="w-full"
+              />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="password" className="font-semibold">
+                Password
+              </Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="••••••••"
+                className="w-full"
+              />
+            </div>
+          </div>
+          <div className="h-3" />
+          <div className="flex w-full justify-end text-sm text-primary">
+            <div>
+              <a className="underline hover:cursor-pointer">Forgot Password?</a>
+            </div>
           </div>
         </CardContent>
-        <CardFooter>
-          <Button className="w-full">Login</Button>
+        <CardFooter className="flex flex-col">
+          <Button className="w-full font-bold">Login</Button>
+          <div className="h-6" />
+          <div className="flex w-full justify-center text-sm font-semibold">
+            <div>Don't have an account?</div>
+            <a className="text-primary underline ml-1 hover:cursor-pointer">
+              Sign up here
+            </a>
+          </div>
         </CardFooter>
       </Card>
     </div>
