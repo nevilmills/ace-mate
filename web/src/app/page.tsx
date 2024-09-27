15740d3b4277381243663da8bd7c12d865bfceae
Nevil Mills <nevilmills@gmail.com>
2024-09-26 19:47:49 -0700

    Protect all routes

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,12 @@
+import { SignedIn, SignOutButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
     </div>
   );
 }
