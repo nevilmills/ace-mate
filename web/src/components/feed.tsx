822783826965d7ecc5d560fcb03cc54fd2a4d0c7
Nevil Mills <nevilmills@gmail.com>
2024-10-02 19:26:20 -0700

    Add link to Create Post button

--- web/src/components/feed.tsx
+++ web/src/components/feed.tsx
@@ -2,33 +2,22 @@
 import React, { useEffect } from "react";
 import { Post } from "./post";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 interface feedProps {}
 
 export const Feed: React.FC<feedProps> = ({}) => {
-  const [creatingPost, setCreatingPost] = React.useState(false);
-  useEffect(() => {
-    console.log("creatingPost state: ", creatingPost);
-  }, [creatingPost]);
-
   return (
     <div className="w-[700px] flex flex-col space-y-4">
       <div className="w-full flex flex-row justify-between">
         <h3 className="text-2xl font-bold pl-6">Your feed</h3>
-        <Button
-          className="text-md font-semibold"
-          onClick={() => setCreatingPost(!creatingPost)}
-        >
-          Post Score
+        <Button asChild className="text-md font-semibold">
+          <Link href="/create-post">Create Post</Link>
         </Button>
       </div>
-      {creatingPost ? null : (
-        <>
-          <Post title="Hello world" />
-          <Post title="Dude I just shot a 79" />
-          <Post title="Is it even worth it bro" />
-        </>
-      )}
+      <Post title="Hello world" />
+      <Post title="Dude I just shot a 79" />
+      <Post title="Is it even worth it bro" />
     </div>
   );
 };
