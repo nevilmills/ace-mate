0e005e265cdf15567e3865d6788c72799f3c3863
Nevil Mills <nevilmills@gmail.com>
2024-10-03 19:54:55 -0700

    Create select-tees component

--- web/src/components/post-creator/post-creator.tsx
+++ web/src/components/post-creator/post-creator.tsx
@@ -2,19 +2,29 @@
 import React, { useEffect, useState } from "react";
 import { SelectCourse } from "./select-course";
 import { CourseSelection } from "@/lib/types";
+import { SelectTees } from "./select-tees";
 
 interface PostCreatorProps {}
 
 export const PostCreator: React.FC<PostCreatorProps> = ({}) => {
   const [course, setCourse] = useState<CourseSelection>(null);
+  const [tees, setTees] = useState<string | null>(null);
 
   useEffect(() => {
     console.log("value: ", course);
   }, [course]);
 
+  if (!course) {
+    return (
+      <div>
+        <SelectCourse setCourse={setCourse} />
+      </div>
+    );
+  }
+
   return (
     <div>
-      <SelectCourse setCourse={setCourse} />
+      <SelectTees setTees={setTees} />
     </div>
   );
 };
---
+++ web/src/components/post-creator/select-tees.tsx
@@ -0,0 +1,127 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import React, { useState } from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { CourseSelection } from "@/lib/types";
+
+interface SelectTeesProps {
+  setTees: React.Dispatch<React.SetStateAction<CourseSelection>>;
+}
+
+const tees = [
+  {
+    value: "Gold",
+    label: "Gold",
+  },
+  {
+    value: "Blue",
+    label: "Blue",
+  },
+  {
+    value: "White",
+    label: "White",
+  },
+  {
+    value: "Green",
+    label: "Green",
+  },
+  {
+    value: "Red",
+    label: "Red",
+  },
+];
+
+export const SelectTees: React.FC<SelectTeesProps> = ({ setTees }) => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState<CourseSelection>(null);
+
+  const handleSubmit = () => {
+    if (!value) return;
+    setTees(value);
+  };
+
+  return (
+    <Card className="w-96 h-52 flex flex-col justify-between">
+      <CardHeader>
+        <CardTitle>Select Tees</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              role="combobox"
+              aria-expanded={open}
+              className="w-full justify-between"
+            >
+              {value
+                ? tees.find((framework) => framework.value === value)?.label
+                : "Choose tees..."}
+              <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[250px] p-0">
+            <Command>
+              <CommandInput placeholder="Search tees..." />
+              <CommandList>
+                <CommandEmpty>No tees found.</CommandEmpty>
+                <CommandGroup>
+                  {tees.map((framework) => (
+                    <CommandItem
+                      key={framework.value}
+                      value={framework.value}
+                      onSelect={(currentValue) => {
+                        setValue(currentValue === value ? "" : currentValue);
+                        setOpen(false);
+                      }}
+                    >
+                      <Check
+                        className={cn(
+                          "mr-2 h-4 w-4",
+                          value === framework.value
+                            ? "opacity-100"
+                            : "opacity-0"
+                        )}
+                      />
+                      {framework.label}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </CommandList>
+            </Command>
+          </PopoverContent>
+        </Popover>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button asChild variant={"outline"}>
+          <Link href="/">Cancel</Link>
+        </Button>
+        <Button className="font-semibold" onClick={handleSubmit}>
+          Next
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
