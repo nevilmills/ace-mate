0c611f514d9ceb675889ae2e430fcd588e36e893
Nevil Mills <nevilmills@gmail.com>
2024-12-07 10:19:35 -0800

    Fix navbar alignment

--- web/src/components/nav-bar.tsx
+++ web/src/components/nav-bar.tsx
@@ -6,7 +6,7 @@ interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
   return (
-    <header className="p-4 px-16 text-primary fixed w-screen h-[92px] z-10 flex items-center bg-[#0a0a0a]">
+    <header className="p-4 px-36 text-primary fixed w-screen h-[92px] z-10 flex items-center bg-[#0a0a0a]">
       <nav className="flex flex-row justify-between items-center grow">
         <div className="flex space-x-4 items-center">
           <div className="h-[50px] w-[50px] rounded-lg overflow-hidden">
