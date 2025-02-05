48c496a09065cfd798bf83ec18324635a16dd423
Nevil Mills <nevilmills@gmail.com>
2025-02-04 20:57:47 -0800

    Fix issue of undefined userId

--- web/src/components/add-friend/add-friend-button.tsx
+++ web/src/components/add-friend/add-friend-button.tsx
@@ -1,6 +1,13 @@
 "use client";
 import React, { useCallback, useState } from "react";
-import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { getUserAutocompletion } from "@/db/queries/user/select";
@@ -11,15 +18,17 @@ import { ExistingUser } from "@/db/schema";
 import { FlagTriangleRight } from "lucide-react";
 import { createUserFriend } from "@/db/queries/user_friends/insert";
 import { deleteUserFriend } from "@/db/queries/user_friends/delete";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 
 interface AddFriendButtonProps {
   friendsMap: Record<string, boolean>;
+  loggedInUser: string;
 }
 
 export const AddFriendButton: React.FC<AddFriendButtonProps> = ({
   friendsMap,
+  loggedInUser,
 }) => {
-  const { userId: loggedInUser } = useAuth();
   const [golfers, setGolfers] = useState<ExistingUser[]>([]);
   const [friends, setFriends] = useState<Record<string, boolean>>(friendsMap);
 
@@ -65,7 +74,13 @@ export const AddFriendButton: React.FC<AddFriendButtonProps> = ({
           + Add friend
         </Button>
       </DialogTrigger>
-      <DialogContent className="bg-[#0a0a0a] max-w-[900px] h-[600px] p-0 overflow-hidden">
+      <DialogContent
+        className="bg-[#0a0a0a] max-w-[900px] h-[600px] p-0 overflow-hidden"
+        aria-describedby={undefined}
+      >
+        <VisuallyHidden>
+          <DialogTitle>Search for golfers</DialogTitle>
+        </VisuallyHidden>
         <div className="flex items-center justify-center h-28 p-8 sticky top-0 bg-card border-b-[1px] border-[rgb(41 37 36)]">
           <Input onChange={handleChange} placeholder="Find golfers" />
         </div>
