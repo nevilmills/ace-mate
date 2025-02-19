beaa150bf3438bf43f0abf345d0a116c40b15682
Nevil Mills <nevilmills@gmail.com>
2025-02-18 20:20:29 -0800

    Add profile skeleton component

---
+++ web/src/components/skeletons/profile-skeleton.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Card, CardContent, CardHeader } from "../ui/card";
+import { Skeleton } from "../ui/skeleton";
+
+interface ProfileSkeletonProps {}
+
+export const ProfileSkeleton: React.FC<ProfileSkeletonProps> = ({}) => {
+  return (
+    <Card className="max-w-[400px] w-[400px]">
+      <CardHeader className="space-y-4">
+        <div className="flex flex-row space-x-4 items-center">
+          <Skeleton className="rounded-full w-[60px] h-[60px]" />
+          <Skeleton className="flex-grow h-6" />
+        </div>
+      </CardHeader>
+      <CardContent>
+        <Skeleton className="h-4 flex-grow mt-4" />
+        <Skeleton className="h-4 flex-grow mt-4" />
+        <Skeleton className="h-4 w-1/2 mt-4" />
+      </CardContent>
+    </Card>
+  );
+};
