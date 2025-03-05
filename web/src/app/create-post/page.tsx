25aacac07e282b9edbaa0f97913ac8e2897bc3f5
Nevil Mills <nevilmills@gmail.com>
2025-03-04 19:06:25 -0800

    Add metadata to create-post route

--- web/src/app/create-post/page.tsx
+++ web/src/app/create-post/page.tsx
@@ -1,10 +1,15 @@
 import { PostCreator } from "@/components/post-creator/post-creator";
 import { getGolfCourses } from "@/db/queries/golf-course/select";
 import React from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Create post | AceMate",
+  icons: "/images/favicon.ico",
+};
 
 const page: React.FC = async () => {
   const golfCourses = await getGolfCourses();
-  console.log(golfCourses);
 
   return (
     <div className="absolute top-0 flex flex-col w-screen h-screen items-center justify-center">
